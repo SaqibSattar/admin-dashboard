d0f88e1dded3015ebbe5380a57745d080b41ae04
SaqibSattar <Saqibkhanz1234@gmail.com>
2023-08-20 19:20:11 +0500

    fixed responsiveness issues

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: 'Widget A',
+    productNumber: 'PRD123',
+    paymentStatus: 'Paid',
+    shipping: 'Shipped',
+  },
+  {
+    productName: 'Gadget B',
+    productNumber: 'PRD456',
+    paymentStatus: 'Pending',
+    shipping: 'Not Shipped',
+  },
+  {
+    productName: 'Accessory C',
+    productNumber: 'PRD789',
+    paymentStatus: 'Paid',
+    shipping: 'Shipped',
+  },
+  {
+    productName: 'Tool D',
+    productNumber: 'PRD234',
+    paymentStatus: 'Paid',
+    shipping: 'Processing',
+  },
+  {
+    productName: 'Device E',
+    productNumber: 'PRD567',
+    paymentStatus: 'Pending',
+    shipping: 'Not Shipped',
+  },
+]
